d44b79ec1b6c42defc2338e9c28012e01d67b464
Olli Savia <ops@iki.fi>
2018-11-07 17:33:01 +0200

    More updates

--- bjr.s
+++ bjr.s
@@ -4,24 +4,7 @@
 ;;; 1985,2018 ops
 ;;;
 
-VICCR0 := VIC+$0
-VICCR1 := VIC+$1
-VICCR2 := VIC+$2
-VICCR3 := VIC+$3
-VICCR4 := VIC+$4
-VICCR5 := VIC+$5
-VICCR6 := VIC+$6
-VICCR7 := VIC+$7
-VICCR8 := VIC+$8
-VICCR9 := VIC+$9
-VICCRA := VIC+$A
-VICCRB := VIC+$B
-VICCRC := VIC+$C
-VICCRD := VIC+$D
-VICCRE := VIC+$E
-VICCRF := VIC+$F
-
-	.setcpu "6502"
+        .setcpu "6502"
 
         .include "cbm_kernal.inc"
         .include "vic20.inc"
@@ -33,12 +16,11 @@ VICCRF := VIC+$F
         ; The following symbol is used by linker config to force the module
         ; to get included into the output file
         .export __EXEHDR__: absolute = 1
-	.segment        "EXEHDR"
+        .segment        "EXEHDR"
 
         .addr   Next
         .word   .version        ; Line number
         .byte   $9E             ; SYS token
-;       .byte   <(((Start / 10000) .mod 10) + '0')
         .byte   <(((Start /  1000) .mod 10) + '0')
         .byte   <(((Start /   100) .mod 10) + '0')
         .byte   <(((Start /    10) .mod 10) + '0')
@@ -69,13 +51,13 @@ Start:
         ldy     #$FF
         jsr     SETLFS
         lda     #fname_end-fname
-	ldx     #<fname
-	ldy     #>fname
+        ldx     #<fname
+        ldy     #>fname
         jsr     SETNAM
         lda     #$00
-	jsr     LOAD
+        jsr     LOAD
 
-	jmp     $2000
+        jmp     $2000
 
-fname:	.byte "loader"
+fname:  .byte "loader"
 fname_end:
